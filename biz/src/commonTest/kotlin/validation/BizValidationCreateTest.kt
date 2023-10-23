e20dca19eb41cc2ea47270686a516ff14c52ce0c
Maru Khan <romirov@gmail.com>
2023-10-23 21:56:49 +0300

    biz logic: fixed tests BizValidation

--- biz/src/commonTest/kotlin/validation/BizValidationCreateTest.kt
+++ biz/src/commonTest/kotlin/validation/BizValidationCreateTest.kt
@@ -3,7 +3,6 @@ package ru.mss.biz.validation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import ru.mss.biz.MssTopicProcessor
 import ru.mss.common.models.MssCommand
-import validation.*
 import kotlin.test.Test
 
 // TODO-validation-5: смотрим пример теста валидации, собранного из тестовых функций-оберток
--- biz/src/commonTest/kotlin/validation/BizValidationUpdateTest.kt
+++ biz/src/commonTest/kotlin/validation/BizValidationUpdateTest.kt
@@ -3,7 +3,6 @@ package ru.mss.biz.validation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import ru.mss.biz.MssTopicProcessor
 import ru.mss.common.models.MssCommand
-import validation.*
 import kotlin.test.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
