a4805ecee25d87f240d71f2ed4a701f0230e258f
Maru Khan <romirov@gmail.com>
2023-12-23 00:03:32 +0300

    refactored ApplicationJvm.kt

--- app-ktor/src/jvmMain/kotlin/ApplicationJvm.kt
+++ app-ktor/src/jvmMain/kotlin/ApplicationJvm.kt
@@ -1,18 +1,10 @@
 package ru.mss.app.ktor
 
-import com.auth0.jwt.JWT
-import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.callloging.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
-import io.ktor.server.websocket.*
 import org.slf4j.event.Level
-import ru.mss.app.common.AuthConfig.Companion.GROUPS_CLAIM
-import ru.mss.app.ktor.base.resolveAlgorithm
 import ru.mss.app.ktor.plugins.initAppSettings
 import ru.mss.app.ktor.plugins.swagger
 import ru.mss.lib.logging.logback.MssLogWrapperLogback
